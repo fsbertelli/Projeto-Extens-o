88b60dd710a6f8738b217935c35e1e220f083283
Felipe Bertelli dos Santos <42683582+fsbertelli@users.noreply.github.com>
2024-11-10 14:13:37 -0300

    Fix main.py
    Fixed page title icon

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 def main():
     st.set_page_config(
-        page_title="🥦 Gerador de Tabela Nutricional 🍝'",
+        page_title="Gerador de Tabela Nutricional'",
         page_icon="🥦",
         initial_sidebar_state="auto",
         menu_items={
             'About': "ADS Unip - 2024"
             }
         )
-    st.header(' Gerador de Tabela Nutricional 🍝')
+    st.header('Gerador de Tabela Nutricional 🍝')
     left, middle = st.columns(2, vertical_alignment="center")
     left.subheader("Projeto de Extensão")
     middle.subheader('ADS Unip 2024')
